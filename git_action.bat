79b5ea829d1471005121716a0f051a891e6d99a9
Ben Newman <bnewman@rocketsoftware.com>
2026-08-17 15:14:06 +0100

    Updated git_action.bat

--- git_action.bat
+++ git_action.bat
@@ -1,7 +1,7 @@
-@echo on
-echo git_action being called here
+@echo off
 git add .
 if errorlevel 1 exit /b %errorlevel%
 git commit -m "%~1"
 if errorlevel 1 exit /b %errorlevel%
 git push
+if errorlevel 1 exit /b %errorlevel%
