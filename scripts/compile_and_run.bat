b4bcde263d39550d1d7bc032680cd88dc0894d0e
Ben Newman <bnewman@rocketsoftware.com>
2026-08-18 15:13:03 +0100

    Updated basic_program.cbl

--- scripts/compile_and_run.bat
+++ scripts/compile_and_run.bat
@@ -4,7 +4,7 @@ cls
 call "C:\\Program Files (x86)\\Rocket Software\\Visual COBOL\\SetupEnv.bat"
 if errorlevel 1 exit /b %errorlevel%
 
-cobol "./cobol/basic_program.cbl" gnt;
+cobol "cobol/basic_program.cbl" gnt;
 if errorlevel 1 (
     echo --- Compilation failed ---
     exit /b 1
@@ -13,5 +13,5 @@ if errorlevel 1 (
 )
 
 echo --- Running 'cobol/basic_program.gnt' ---
-run "./cobol/basic_program.gnt"
+run "cobol/basic_program.gnt"
 if errorlevel 1 exit /b %errorlevel%
