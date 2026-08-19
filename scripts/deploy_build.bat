afa49956517771d6e22490e5788f8c7305f76b89
Ben Newman <bnewman@rocketsoftware.com>
2026-08-19 10:46:42 +0100

    Updated deploy_build.bat

--- scripts/deploy_build.bat
+++ scripts/deploy_build.bat
@@ -1,11 +1,26 @@
-if not exist "C:/Users/bnewman/Documents/Dev/TasksCICD/builds" (
-    mkdir "C:/Users/bnewman/Documents/Dev/TasksCICD/builds"
+@REM if not exist "C:/Users/bnewman/Documents/Dev/TasksCICD/builds" (
+@REM     mkdir "C:/Users/bnewman/Documents/Dev/TasksCICD/builds"
+@REM )
+
+@REM if exist "cobol/basic_program.gnt" (
+@REM     echo "Found cobol/basic_program.gnt"
+@REM     if exist "C:/Users/bnewman/Documents/Dev/TasksCICD/builds" (
+@REM         echo "Found C:/Users/bnewman/Documents/Dev/TasksCICD/builds"
+@REM         copy "cobol/basic_program.gnt" "C:/Users/bnewman/Documents/Dev/TasksCICD/builds/basic_program.gnt"
+@REM     ) else (exit /b 1)
+@REM ) else (exit /b 1)
+
+
+echo off
+
+if not exist "builds" (
+    mkdir "builds"
 )
 
-if exist "cobol/basic_program.gnt" (
-    echo "Found cobol/basic_program.gnt"
-    if exist "C:/Users/bnewman/Documents/Dev/TasksCICD/builds" (
-        echo "Found C:/Users/bnewman/Documents/Dev/TasksCICD/builds"
-        copy "cobol/basic_program.gnt" "C:/Users/bnewman/Documents/Dev/TasksCICD/builds/basic_program.gnt"
+if exist "cobol\basic_program.gnt" (
+    echo "Found cobol\basic_program.gnt"
+    if exist "builds" (
+        echo "Found builds"
+        copy /Y "cobol\basic_program.gnt" "builds\basic_program.gnt"
     ) else (exit /b 1)
 ) else (exit /b 1)
