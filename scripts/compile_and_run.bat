760f81bf6b586b24537b1a2bdb071f0f0cb13d88
Ben Newman <bnewman@rocketsoftware.com>
2026-08-19 11:06:05 +0100

    Changed / to "

--- scripts/compile_and_run.bat
+++ scripts/compile_and_run.bat
@@ -1,10 +1,10 @@
 echo off
 cls
 
-call "C:\\Program Files (x86)\\Rocket Software\\Visual COBOL\\SetupEnv.bat"
+call "C:\Program Files (x86)\Rocket Software\Visual COBOL\SetupEnv.bat"
 if errorlevel 1 exit /b %errorlevel%
 
-cobol "cobol/basic_program.cbl" gnt;
+cobol "cobol\basic_program.cbl" gnt;
 if errorlevel 1 (
     echo --- Compilation failed ---
     exit /b 1
@@ -12,6 +12,6 @@ if errorlevel 1 (
     echo --- Compilation successful ---
 )
 
-echo --- Running 'cobol/basic_program.gnt' ---
-run "cobol/basic_program.gnt"
+echo --- Running 'cobol\basic_program.gnt' ---
+run "cobol\basic_program.gnt"
 if errorlevel 1 exit /b %errorlevel%
--- scripts/deploy_build.bat
+++ scripts/deploy_build.bat
@@ -8,9 +8,9 @@ if not exist "%DEPLOY_DIR%" (
 )
 
 if exist "%BUILD_FILE%" (
-    echo "Found %BUILD_FILE%"
+    @REM echo "Found %BUILD_FILE%"
     if exist "%DEPLOY_DIR%" (
-        echo "Found %DEPLOY_DIR%"
+        @REM echo "Found %DEPLOY_DIR%"
         copy /Y "%BUILD_FILE%" "%DEPLOY_DIR%\name_of_gnt.gnt"
     ) else (
         echo "Failed to find %DEPLOY_DIR%"
@@ -20,18 +20,3 @@ if exist "%BUILD_FILE%" (
     echo "Failed to find %BUILD_FILE%"
     exit /b 1
 )
-
-
-@REM echo off
-
-@REM if not exist "builds" (
-@REM     mkdir "builds"
-@REM )
-
-@REM if exist "cobol\basic_program.gnt" (
-@REM     echo "Found cobol\basic_program.gnt"
-@REM     if exist "builds" (
-@REM         echo "Found builds"
-@REM         copy \Y "cobol\basic_program.gnt" "builds\basic_program.gnt"
-@REM     ) else (exit \b 1)
-@REM ) else (exit \b 1)
