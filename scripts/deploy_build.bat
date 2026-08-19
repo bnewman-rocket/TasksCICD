7d7f2bcfa8f11fbac1a5852074f7fce28b5139ce
Ben Newman <bnewman@rocketsoftware.com>
2026-08-19 10:34:43 +0100

    Updated deploy_build.bat

--- scripts/deploy_build.bat
+++ scripts/deploy_build.bat
@@ -3,5 +3,6 @@ if not exist "C:/Users/bnewman/Documents/Dev/TasksCICD/builds" (
 )
 
 if exist "cobol/basic_program.gnt" (
-    copy "cobol/basic_program.gnt" "builds"
+    @REM copy "cobol/basic_program.gnt" "builds"
+    echo "Found cobol/basic_program.gnt"
 ) else (exit /b 1)
