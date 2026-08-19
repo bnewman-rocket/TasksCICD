1d5e2e2d0b335054e3c9bd1d34af96dc2bf7e683
Ben Newman <bnewman@rocketsoftware.com>
2026-08-19 10:07:54 +0100

    Updated test_for_string.bat

--- scripts/test_for_string.bat
+++ scripts/test_for_string.bat
@@ -2,8 +2,8 @@ echo off
 @REM findstr /C:"Hello, World!" output.txt
 findstr /C:"%~1" output.txt
 if errorlevel 1 (
-    echo TEST FAILED: Expected output was not found.
+    echo TEST FAILED: "%~1" was not found.
     exit /b 1
 ) else (
-    echo TEST PASSED: Expected output was found.
+    echo TEST PASSED: "%~1" was found.
 )
