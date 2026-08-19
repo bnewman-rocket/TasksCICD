79c17aa83d7e920af78907dc9cefd36c33805323
Ben Newman <bnewman@rocketsoftware.com>
2026-08-19 11:21:24 +0100

    Updated basic_program.cbl

--- cobol/basic_program.cbl
+++ cobol/basic_program.cbl
@@ -13,7 +13,9 @@
            perform main
            .
        main section.
-           display "Hello, World!"
+      *    display "Hello, World!"
+           display "What is happening? Hello"
+           display ", World!"
            .
            goback
            .
