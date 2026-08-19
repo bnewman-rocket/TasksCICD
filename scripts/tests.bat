8948d8281687af7e2d20cc60eb259a79372152a6
Ben Newman <bnewman@rocketsoftware.com>
2026-08-19 10:11:13 +0100

    Added tests directory

--- scripts/tests.bat
+++
@@ -1,2 +0,0 @@
-call "scripts/test_for_string.bat" "This should fail"
-call "scripts/test_for_string.bat" "Hello, World!"
---
+++ tests/tests.bat
@@ -0,0 +1,2 @@
+call "tests/test_for_string.bat" "This should fail"
+call "tests/test_for_string.bat" "Hello, World!"
